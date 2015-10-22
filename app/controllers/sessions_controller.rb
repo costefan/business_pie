2b7ba13c11ea0f407be1cad738b1f9772bcd5035
costefan <costefan@yandex.ru>
2015-10-23 01:44:38 +0300

    added edit and index plus good forwarding

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      sign_in user
+      flash[:success] = "Ok"
+      redirect_back_or user
+    else
+      flash.now[:error] = "Invalid email/password pair"
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+
+end
