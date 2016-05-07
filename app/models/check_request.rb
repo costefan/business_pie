530e7b1a08e973432f0d25235eadcc8939b8d3ba
costefan <costefan@yandex.ru>
2016-05-07 20:22:24 +0300

    Added reports storage

--- app/models/check_request.rb
+++ app/models/check_request.rb
@@ -2,7 +2,9 @@ class CheckRequest
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :location, type: Point
+  field :loc, :type => Array
+  index({ loc: '2d' }, { min: -200, max: 200 })
+
   has_one :user, foreign_key: :user_id, class_name: 'User'
   embeds_one :request_type, class_name: 'RequestType'
   index({ 'request_type.name' => 1}, { unique: true })
--- app/models/search_request.rb
+++ app/models/search_request.rb
@@ -2,7 +2,9 @@ class SearchRequest
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :center, type: Point
+  field :loc, :type => Array
+  index({ loc: '2d' }, { min: -200, max: 200 })
+
   field :radius, type: Float
   has_one :user, foreign_key: :user_id, class_name: 'User'
   embeds_one :request_type, class_name: 'RequestType'
