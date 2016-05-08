36a49f41c0f7aa956db3f703930d437f21ad1b73
Maxym Bulgar <Maxym Bulgar>
2016-05-08 15:00:13 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#User.create(name: 'admin', email: "admin@gmail.com", password: "qwerty123", password_confirmation: "qwerty213", admin: true)
+# User.delete_all
+# RequestType.delete_all
+# user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'qwerty123', :password_confirmation => 'qwerty123'
+# user2 = User.create! :name => 'Admin', :email => 'admin@example.com', :password => 'qwerty123', :password_confirmation => 'qwerty123' ,:admin => true
+request_type1 = RequestType.create! :name => 'Restourant', :keywords => ["cafe", "restourant"]	
