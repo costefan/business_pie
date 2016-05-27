4fe6be7fe1c198c701894588d416a38d9effa93d
Cemal96 <maksbulgar@gmail.com>
2016-05-27 04:40:39 +0300

    Add new dashboard

--- app/controllers/check_requests_controller.rb
+++ app/controllers/check_requests_controller.rb
@@ -4,11 +4,11 @@ class CheckRequestsController < ApplicationController
 	end
 
 	def create
-		@result = HTTParty.post("http://192.168.131.253:8080/users/#{current_user.id}/check_request", 
-		{ 
-		    :body => params[:check_request].to_json,
-		    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-		})
+		# @result = HTTParty.post("http://192.168.131.253:8080/users/#{current_user.id}/check_request", 
+		# { 
+		#     :body => params[:check_request].to_json,
+		#     :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+		# })
 		redirect_to root_path
 	end
 end
