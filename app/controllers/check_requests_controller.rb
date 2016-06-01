4db127588cc2f400089ea8d9df99d6e6fde9b983
Cemal96 <maksbulgar@gmail.com>
2016-06-01 05:37:03 +0300

    synchronization with py-server

--- app/controllers/check_requests_controller.rb
+++ app/controllers/check_requests_controller.rb
@@ -5,6 +5,8 @@ class CheckRequestsController < ApplicationController
 	end
 
 	def create
+		params[:check_request][:center][0] = params[:check_request][:center][0].to_f
+		params[:check_request][:center][1] = params[:check_request][:center][1].to_f
 		current_user.spend_service("check_request")
 		@result = HTTParty.post("http://192.168.1.102:8080/users/#{current_user.id}/check_request", 
 		{ 
@@ -13,6 +15,8 @@ class CheckRequestsController < ApplicationController
 		})
 
 		redirect_to root_path
+		# params[:user_id] = current_user.id[:id]
+		# render "static_pages/home"
 	end
 	private
  	def major
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,11 @@
 # RequestType.delete_all
 # user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'qwerty123', :password_confirmation => 'qwerty123'
 user2 = User.create! :name => 'Admin', :email => 'admin@admin.com', :password => 'qwerty123C', :password_confirmation => 'qwerty123C' ,:admin => true
-request_type1 = RequestType.create! :name => 'Restourant', :keyword => "cafe,restourant,bar,carryout,coffeshop,pizzeria,teahouse,winebar,snack,pop-up,snackbar,pizza,grill,eatery,coffehouse",
- :positive_verb => "tasty,good,delicious", :negative_verb => "bad,nottasty,ugly,tasteless,inedible,sour,stale,bland,unpalatable"
+request_type1 = RequestType.create! :name => 'Restourant', :keyword => "cafe restourant bar carryout coffeshop pizzeria teahouse winebar snack pop-up snackbar pizza grill eatery coffehouse localfood vegan vegetarian foodie eat lunch veganfood vegetables",
+ :positive_verb => "tasty good delicious instalike awesome happiness enjoylife mustvisit foodlover healthyfood happy  healthylife healthy blogger", :negative_verb => "bad nottasty ugly tasteless inedible sour stale bland unpalatable shit junkfood shitfood junk nutrition fuck sad"
 
  # tasty good delicious instalike awesome happiness enjoylife mustvisit foodlover healthyfood happy  healthylife healthy blogger
  # bad nottasty ugly tasteless inedible sour stale bland unpalatable shit junkfood shitfood junk nutrition fuck sad
  # Restourant
  # cafe restourant bar carryout coffeshop pizzeria teahouse winebar snack pop-up snackbar pizza grill eatery coffehouse localfood vegan vegetarian foodie eat lunch veganfood vegetables
+ 
